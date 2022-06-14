c4b65fcf375d147fe83e99ab82e2d0c9c7e7c1a7
Liam C <thecouchcushen@gmail.com>
2022-06-14 11:27:29 -0400

    Added contact me scaffolding

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,33 @@
+import email_img from "../email-outline.svg"
+import github_img from "../github.svg"
+import twitter_img from "../twitter-original.svg"
+import "./Contact.css"
+
 const Contact = (props) => {
 
     return (
         <section id="contact">
+            <h3>Contact</h3>
+            <a className="contact-link" href="https://github.com/thecouchcushen" target="_blank" rel="noreferrer"><img className="contact-logo" id="github-logo" src={github_img} alt="github logo" /></a>
+            <a className="contact-link" href="https://twitter.com/the_flossiest" target="_blank" rel="noreferrer"><img className="contact-logo" src={twitter_img} alt="twitter logo" /></a>
+
+            
+            <a  className="contact-link" href="mailto:thecouchcushen@gmail.com">
+                <img className="contact-logo" id="email-logo" src={email_img} alt="mail logo" />
+                <p>thecouchcushen@gmail.com</p>
+            </a>
+            
+            <iframe
+                title="Office Location"
+                width="450"
+                height="300"
+                style={{"border":0}}
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyArWAQp38sNWsx1PAUV0GXRLcXeB07zhjE&q=30+Rockefeller+Plaza, New York+ New York"
+                >
+            </iframe>
+            {/*
             <form>
-            <h2>Contact Me</h2>
+            
 
             <label htmlFor="personName">Name</label>
             <input name="personName" type="text"></input>
@@ -16,6 +40,7 @@ const Contact = (props) => {
             
             <button>Submit</button>
             </form>
+    */}
         </section>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,12 +11,15 @@ const Projects = (props) => {
             <div className='projects-wrapper'>
             {projects.map((project, i) => 
             <div className='project-wrapper' key={"Project"+i}>
+                <img src={process.env.PUBLIC_URL + project.image} alt="cant locate"></img>
+                <div className='project-information-display'>
                 <p><strong>{project.title}</strong></p>
                 <a href={project.link} target="_blank" rel="noreferrer"><img src={webLogo} alt="cant find web icon"></img>Site</a>
                 <a href={project.repo} target="_blank" rel="noreferrer"><img src={githubLogo} alt="cant find github icon"></img>Repo</a>
-                <img src={project.image} alt="not loaded yet"></img>
+                
                 <p>{project.subtitle}</p>
                 <p>{project.description}</p>
+                </div>
             </div>
             )}
             </div>
